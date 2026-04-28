a6125461001f4c2eaa467c86b72e5473c4fe1edd
sugar786 <tangmiao021030@163.com>
2026-04-28 10:31:57 +0800

    Create etth1.sh

---
+++ scripts/etth1.sh
@@ -0,0 +1,43 @@
+ma_type=ema
+alpha=0.3
+beta=0.3
+
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/"$ma_type ]; then
+    mkdir ./logs/$ma_type
+fi
+
+model_name=xPatch
+seq_len=96
+
+for model_name in xPatch
+do
+for pred_len in 96 192 336 720
+do
+  python -u run.py \
+    --is_training 1 \
+    --root_path ./dataset/ \
+    --data_path ETTh1.csv \
+    --model_id ETTh1_$pred_len'_'$ma_type \
+    --model $model_name \
+    --data ETTh1 \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in 7 \
+    --des 'Exp' \
+    --itr 1 \
+    --batch_size 512 \
+    --learning_rate 0.0005 \
+    --lradj 'sigmoid'\
+    --ma_type $ma_type \
+    --alpha $alpha \
+    --use_trend_interactor 1 \
+    --topk 2 \
+    --interactor_dropout 0.0\
+    --beta $beta > logs/$ma_type/$model_name'_ETTh1_'$seq_len'_'$pred_len.log
+done
+done
